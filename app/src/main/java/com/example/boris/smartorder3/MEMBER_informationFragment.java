01d0435cbccf4c923bac7685c61ba44f0c52b840
kimbely <kimbely0320@gmail.com>
2018-09-07 21:47:28 +0800

    阿梅0.1版

--- app/src/main/java/com/example/boris/smartorder3/MEMBER_informationFragment.java
+++ app/src/main/java/com/example/boris/smartorder3/MEMBER_informationFragment.java
@@ -12,8 +12,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,8 +25,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MEMBER_informationFragment extends Fragment {
     private RecyclerView recyclerView;
@@ -44,13 +50,55 @@ public class MEMBER_informationFragment extends Fragment {
     private void handleview(View view) {
         recyclerView=view.findViewById(R.id.reinformation);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(new NormalRecyclerViewAdapter(getActivity(),getContext()));
+        recyclerView.setAdapter(new ItemAdapter(getRecyclerView_Item(),getContext()));
 
     }
 
+    private class ItemAdapter extends RecyclerView.Adapter<MEMBER_informationFragment.ItemAdapter.MyViewHolder> {
+        List<RecyclerView_Item> items;
+        Context context;
+
+        public ItemAdapter(List<RecyclerView_Item> items, Context context) {
+            this.items = items;
+            this.context = context;
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+            TextView tvName;
+
+            public MyViewHolder(View item_view) {
+                super(item_view);
+
+            }
+        }
+
+        public MEMBER_informationFragment.ItemAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View item_view = layoutInflater.inflate(R.layout.member_show_information, parent, false);
+            return new MEMBER_informationFragment.ItemAdapter.MyViewHolder(item_view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        }
 
 
+    }
 
+    private List<RecyclerView_Item> getRecyclerView_Item() {
+        List<RecyclerView_Item> Items = new ArrayList<>();
+
+        Items.add(new RecyclerView_Item(1, 2222));
+
+        return Items;
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/example/boris/smartorder3/MEMBER_orderlistFragment.java
+++ app/src/main/java/com/example/boris/smartorder3/MEMBER_orderlistFragment.java
@@ -1,23 +1,86 @@
 package com.example.boris.smartorder3;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MEMBER_orderlistFragment extends Fragment {
 
+    private RecyclerView orderrecyclerView;
+
     public static Fragment newInstance(){
-        MEMBER_orderlistFragment fragment = new MEMBER_orderlistFragment();
-        return fragment;
+        MEMBER_orderlistFragment orderfragment = new MEMBER_orderlistFragment();
+        return orderfragment;
     }
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.member_orderlistfragment,null);
+    public View onCreateView(LayoutInflater ordrtinflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = ordrtinflater.inflate(R.layout.member_orderlistfragment,null);
+        Review(view);
         return view;
+    }
+
+    private void Review(View view) {
+        orderrecyclerView=view.findViewById(R.id.reorderlist);
+        orderrecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        orderrecyclerView.setAdapter(new MEMBER_orderlistFragment.orderItemAdapter(getRecyclerView_Item(),getContext()));
+
+    }
+
+    private class orderItemAdapter extends RecyclerView.Adapter<MEMBER_orderlistFragment.orderItemAdapter.MyViewHolder> {
+        List<RecyclerView_Item> items;
+        Context context;
+
+        public orderItemAdapter(List<RecyclerView_Item> items, Context context) {
+            this.items = items;
+            this.context = context;
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+            TextView tvName;
+
+            public MyViewHolder(View item_view) {
+                super(item_view);
+
+            }
+        }
+
+        public MEMBER_orderlistFragment.orderItemAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View item_view = layoutInflater.inflate(R.layout.member_show_orderlist, parent, false);
+            return new MEMBER_orderlistFragment.orderItemAdapter.MyViewHolder(item_view);
+        }
+        public void onBindViewHolder(@NonNull MEMBER_orderlistFragment.orderItemAdapter.MyViewHolder holder, int position) {
+
+        }
+
+
+
+    }
+
+    private List<RecyclerView_Item> getRecyclerView_Item() {
+        List<RecyclerView_Item> Items = new ArrayList<>();
+
+        Items.add(new RecyclerView_Item(1, 2222));
+        return Items;
     }
 
     @Override
@@ -30,5 +93,3 @@ public class MEMBER_orderlistFragment extends Fragment {
         super.onDestroyView();
     }
 }
-
-
