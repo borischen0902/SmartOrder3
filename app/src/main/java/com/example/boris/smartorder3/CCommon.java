6ae76b1f88455c8c349595897ee4758001843550
Boris <borischen0902@gmail.com>
2018-09-10 16:46:36 +0800

    MainActivity -> Login連接Web ok, 儲存使用者設定 ok (未移除直接登入頁面鍵)

--- app/src/main/java/com/example/boris/smartorder3/CCommon.java
+++ app/src/main/java/com/example/boris/smartorder3/CCommon.java
@@ -8,14 +8,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 public class CCommon {
-    public static final String URL = "http://10.0.2.2:8080";
+    public final static String LOGIN_INFO = "SmartOrderLoginInfo";
+    public final static  String URL = "http://10.0.2.2:8080/SmartOrder_Web";
 
     public static boolean isNetworkConnected(Activity activity) {
         NetworkInfo networkInfo;
         ConnectivityManager connectivityManager = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
         networkInfo = connectivityManager != null ? connectivityManager.getActiveNetworkInfo() : null;
         return (networkInfo != null) && networkInfo.isConnected();
-
     }
 
 }
---
+++ app/src/main/java/com/example/boris/smartorder3/CCommonTask.java
@@ -0,0 +1,64 @@
+package com.example.boris.smartorder3;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CCommonTask extends AsyncTask<String, Integer, String> {
+    private final String TAG = "CCoommonTask";
+    private String url, outStr;
+
+    public CCommonTask(String url, String outStr) {
+        this.url = url;
+        this.outStr = outStr;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        return getRemoteDate();
+    }
+
+    private String getRemoteDate() {
+        HttpURLConnection con = null;
+        StringBuilder inStr = new StringBuilder();
+        try {
+            con = (HttpURLConnection) new URL(url).openConnection();
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setChunkedStreamingMode(0);
+            con.setUseCaches(false);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("charset", "UTF-8");
+            BufferedWriter bw = new BufferedWriter((new OutputStreamWriter((con.getOutputStream()))));
+            bw.write(outStr);
+            Log.d(TAG, "output:" + outStr);
+            bw.close();
+            int responseCode = con.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    inStr.append(line);
+                }
+            } else {
+                Log.d(TAG, "response code:" + responseCode);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
+        }
+        Log.d(TAG, "input:" + inStr);
+        return inStr.toString();
+
+    }
+}
