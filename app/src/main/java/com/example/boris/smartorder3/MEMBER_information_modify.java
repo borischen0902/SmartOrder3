7894a8408cb9dfa6f9c03e4e63c1bbaf4f2c354d
kimbely <kimbely0320@gmail.com>
2018-09-18 16:26:25 +0800

    阿梅2.4.2 美化介面

--- app/src/main/java/com/example/boris/smartorder3/MEMBER_information_modify.java
+++ app/src/main/java/com/example/boris/smartorder3/MEMBER_information_modify.java
@@ -2,7 +2,9 @@ package com.example.boris.smartorder3;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -26,19 +28,46 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.widget.DatePicker;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-public class MEMBER_information_modify extends AppCompatActivity {
+public class MEMBER_information_modify extends AppCompatActivity  {
 
-
-
+    private int mYear, mMonth, mDay;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_show_modify_information);
+
+
+        final Button dateButton = (Button)findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                new DatePickerDialog(MEMBER_information_modify.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        String format =  setDateFormat(year, month, day);
+                        dateButton.setText(format);
+                    }
+
+                }, mYear,mMonth, mDay).show();
+            }
+        });
+
+
         Button btok = findViewById(R.id.btOK);
         btok .setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,11 +87,22 @@ public class MEMBER_information_modify extends AppCompatActivity {
 
     }
 
+
+
+    private String setDateFormat(int year,int monthOfYear,int dayOfMonth){
+        return String.valueOf(year) + "-"
+                + String.valueOf(monthOfYear + 1) + "-"
+                + String.valueOf(dayOfMonth);
+    }
+
+
+
     public void refresh() {
         finish();
         Intent intent = new Intent(this,MEMBER_information_modify.class);
         startActivity(intent);
     }
+
 
 }
 
